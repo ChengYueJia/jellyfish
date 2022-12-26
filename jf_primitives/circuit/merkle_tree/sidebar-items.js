b9ebf653c3de7bfc0fb6b7ff1e3974903f3cc769
ChengYueJia <ChengYueJia@users.noreply.github.com>
2022-12-26 07:17:41 +0000

    deploy: f88c7145cbed1dd68cd09d267ce7cda8221c772a

---
+++ jf_primitives/circuit/merkle_tree/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"trait":[["MerkleTreeGadget","Gadget for a Merkle tree"]]};
---
+++ jf_primitives/circuit/prf/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"trait":[["PrfGadget","Circuit implementation of a PRF."]]};
